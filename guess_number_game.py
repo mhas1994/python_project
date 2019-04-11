d6bbbc269c0ad871bad3d753fa219c144ea39ac0
name <u@example.com>
2019-04-12 00:18:45 +0500

    first commit

---
+++ guess_number_game.py
@@ -0,0 +1,23 @@
+"""Guess a number within a range.
+
+from random import randint
+
+start = 1
+end = 100
+value = randint(start, end)
+
+print(value)
+print("I'm thinking of a number between", start, "and", end)
+
+guess = None
+
+while guess != value:
+    text = input("Guess the number: ")
+    guess = int(text)
+
+    if guess < value:
+        print("Higher.")
+    elif guess > value:
+        print("Lower.")
+
+print("Congratulations! You guessed the right answer:", value)
